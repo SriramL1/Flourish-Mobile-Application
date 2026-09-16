6f9639a987c805a900b131972236f27c0655aebd
Sriram Lakamsani <sriraml1@Srirams-MacBook-Pro.local>
2026-09-15 20:11:16 -0500

    FLR-8 New changes for testing

--- Status.py
+++ Status.py
@@ -1,9 +1,9 @@
+value = 0  # If you want type hints: value: int = 0
 
-int value = 0
-if (value < 0):
+if value < 0:
     print("Value is negative")
-else if (value == 0):
-    print("Value is zero") 
+elif value == 0:
+    print("Value is zero")
 else:
     print("Value is positive")
-#
+# End of if-else block
