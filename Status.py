8dd55d3303a4851a23dae8104ac4b958e79689d9
Sriram Lakamsani <srir5182@gmail.com>
2026-09-22 01:06:21 -0500

    FLR-7 new branch for adding more checking code (#8)
    * FLR-8 wrong code run:
    
    * FLR-8 new addition to failure for test run
    
    * FLR-8 New changes for testing
    
    * FLR-7 New code for checking between frontend and backend
    
    * FLR-7 New changes to code
    
    * FLR-7 Add backend requirements.txt and fix CI workflow
    
    * FLR-7 Remove premature test steps, fix lint command and cache path

---
+++ Status.py
@@ -0,0 +1,9 @@
+value = 0  # If you want type hints: value: int = 0
+
+if value < 0:
+    print("Value is negative")
+elif value == 0:
+    print("Value is zero")
+else:
+    print("Value is positive")
+# End of if-else block
